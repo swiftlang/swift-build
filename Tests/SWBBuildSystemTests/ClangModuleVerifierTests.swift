01a1d91c09ea2904e707328469e4a3c03154b038
Steven Wu <stevenwu@apple.com>
2025-03-13 14:03:26 -0700

    [TEST-ONLY] Partial revert #302
    Unfortunately, caching tests are still flaky in some CI infrastructure.
    Partially revert by keeping the `.flaky` tags but leaving the
    test fixes.

--- Tests/SWBBuildSystemTests/ClangModuleVerifierTests.swift
+++ Tests/SWBBuildSystemTests/ClangModuleVerifierTests.swift
@@ -191,7 +191,8 @@ fileprivate struct ClangModuleVerifierTests: CoreBasedTests {
         }
     }
 
-    @Test(.requireSDKs(.macOS), .requireClangFeatures(.wSystemHeadersInModule), .requireDependencyScannerPlusCaching, .requireCompilationCaching)
+    @Test(.requireSDKs(.macOS), .requireClangFeatures(.wSystemHeadersInModule), .requireDependencyScannerPlusCaching, .requireCompilationCaching,
+          .flaky("A handful of Swift Build CAS tests fail when running the entire test suite"), .bug("rdar://146781403"))
     func cachedBuild() async throws {
         try await withTemporaryDirectory { (tmpDirPath: Path) in
             let archs = ["arm64", "x86_64"]
