b51f81214322c6b5b9b04ce0950ea883923f14ed
Owen Voorhees <ovoorhees@apple.com>
2025-06-02 09:07:10 -0700

    Revert "[6.2] Update the swift-driver to release/6.2 branch (#407)"
    This reverts commit 65eed717bb19da35f6587613130bb4f9e6801d03.

--- Package.swift
+++ Package.swift
@@ -455,7 +455,7 @@ if useLocalDependencies {
     }
 } else {
     package.dependencies += [
-        .package(url: "https://github.com/swiftlang/swift-driver.git", branch: "release/6.2"),
+        .package(url: "https://github.com/swiftlang/swift-driver.git", branch: "main"),
         .package(url: "https://github.com/apple/swift-system.git", .upToNextMajor(from: "1.4.1")),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.3"),
     ]
