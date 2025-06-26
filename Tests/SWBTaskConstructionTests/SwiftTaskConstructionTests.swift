01a7c3d87b236609e21259c4097f5a8f427d9424
Alexis Laferrière <alaf@apple.com>
2025-06-26 13:43:55 -0700

    Don't require objc in modulemaps generated for Swift compatibility headers
    Let's remove the `requires objc` from the module map file generated for
    the compatibility header. This condition triggered an error when
    importing the compatibility header from a C source file.
    
    This declaration is superfluous, the compatibility header is already
    printed in a way where the Objective-C code is protected behind a
    language check. C clients can safely import the current compatibility
    header even if they may not see any content.
    
    Let's lift this restriction. It isn't currently necessary and we're
    adding C content to the compatibility header with the official support
    for `@cdecl` that is independent of Objective-C.

--- Tests/SWBTaskConstructionTests/SwiftTaskConstructionTests.swift
+++ Tests/SWBTaskConstructionTests/SwiftTaskConstructionTests.swift
@@ -606,7 +606,6 @@ fileprivate struct SwiftTaskConstructionTests: CoreBasedTests {
                 #expect(contents == (OutputByteStream()
                                      <<< "framework module CoreFoo {\n"
                                      <<< "  header \"CoreFoo-Swift.h\"\n"
-                                     <<< "  requires objc\n"
                                      <<< "}\n").bytes)
             }
         }
@@ -1086,7 +1085,6 @@ fileprivate struct SwiftTaskConstructionTests: CoreBasedTests {
                     stream <<< "\n"
                     stream <<< "module CoreFoo.Swift {\n"
                     stream <<< "  header \"CoreFoo-Swift.h\"\n"
-                    stream <<< "  requires objc\n"
                     stream <<< "}\n"
                     #expect(contents == stream.bytes)
 
