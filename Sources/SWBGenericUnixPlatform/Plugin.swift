9000ce167e4f1f9b8696b63cb4b75822f3f30b25
Jake Petroules <jake.petroules@apple.com>
2025-08-04 10:58:45 -0700

    Handle the OS version in the triple for FreeBSD
    We're going to keep the versioned triple, so it needs to be passed through after all.

--- Sources/SWBGenericUnixPlatform/Plugin.swift
+++ Sources/SWBGenericUnixPlatform/Plugin.swift
@@ -15,11 +15,44 @@ import SWBCore
 import Foundation
 
 @PluginExtensionSystemActor public func initializePlugin(_ manager: PluginManager) {
+    let plugin = GenericUnixPlugin()
     manager.register(GenericUnixDeveloperDirectoryExtension(), type: DeveloperDirectoryExtensionPoint.self)
     manager.register(GenericUnixPlatformSpecsExtension(), type: SpecificationsExtensionPoint.self)
     manager.register(GenericUnixPlatformInfoExtension(), type: PlatformInfoExtensionPoint.self)
-    manager.register(GenericUnixSDKRegistryExtension(), type: SDKRegistryExtensionPoint.self)
-    manager.register(GenericUnixToolchainRegistryExtension(), type: ToolchainRegistryExtensionPoint.self)
+    manager.register(GenericUnixSDKRegistryExtension(plugin: plugin), type: SDKRegistryExtensionPoint.self)
+    manager.register(GenericUnixToolchainRegistryExtension(plugin: plugin), type: ToolchainRegistryExtensionPoint.self)
+}
+
+final class GenericUnixPlugin: Sendable {
+    func swiftExecutablePath(fs: any FSProxy) -> Path? {
+        [
+            Environment.current["SWIFT_EXEC"].map(Path.init),
+            StackedSearchPath(environment: .current, fs: fs).lookup(Path("swift"))
+        ].compactMap { $0 }.first(where: fs.exists)
+    }
+
+    func swiftTargetInfo(swiftExecutablePath: Path) async throws -> SwiftTargetInfo {
+        let args = ["-print-target-info"]
+        let executionResult = try await Process.getOutput(url: URL(fileURLWithPath: swiftExecutablePath.str), arguments: args)
+        guard executionResult.exitStatus.isSuccess else {
+            throw RunProcessNonZeroExitError(args: [swiftExecutablePath.str] + args, workingDirectory: nil, environment: [:], status: executionResult.exitStatus, stdout: ByteString(executionResult.stdout), stderr: ByteString(executionResult.stderr))
+        }
+        return try JSONDecoder().decode(SwiftTargetInfo.self, from: executionResult.stdout)
+    }
+}
+
+struct SwiftTargetInfo: Decodable {
+    struct TargetInfo: Decodable {
+        let triple: LLVMTriple
+        let unversionedTriple: LLVMTriple
+    }
+    let target: TargetInfo
+}
+
+extension SwiftTargetInfo.TargetInfo {
+    var tripleVersion: String? {
+        triple != unversionedTriple && triple.system.hasPrefix(unversionedTriple.system) ? String(triple.system.dropFirst(unversionedTriple.system.count)).nilIfEmpty : nil
+    }
 }
 
 struct GenericUnixDeveloperDirectoryExtension: DeveloperDirectoryExtension {
@@ -69,9 +102,11 @@ struct GenericUnixPlatformInfoExtension: PlatformInfoExtension {
 }
 
 struct GenericUnixSDKRegistryExtension: SDKRegistryExtension {
+    let plugin: GenericUnixPlugin
+
     func additionalSDKs(context: any SDKRegistryExtensionAdditionalSDKsContext) async throws -> [(path: Path, platform: SWBCore.Platform?, data: [String: PropertyListItem])] {
         let operatingSystem = context.hostOperatingSystem
-        guard operatingSystem.createFallbackSystemToolchain, let platform = try context.platformRegistry.lookup(name: operatingSystem.xcodePlatformName) else {
+        guard operatingSystem.createFallbackSystemToolchain, let platform = try context.platformRegistry.lookup(name: operatingSystem.xcodePlatformName), let swift = plugin.swiftExecutablePath(fs: context.fs) else {
             return []
         }
 
@@ -100,6 +135,23 @@ struct GenericUnixSDKRegistryExtension: SDKRegistryExtension {
             tripleEnvironment = ""
         }
 
+        let swiftTargetInfo = try await plugin.swiftTargetInfo(swiftExecutablePath: swift)
+
+        let deploymentTargetSettings: [String: PropertyListItem]
+        if operatingSystem == .freebsd {
+            guard let tripleVersion = swiftTargetInfo.target.tripleVersion else {
+                throw StubError.error("Unknown FreeBSD triple version")
+            }
+            deploymentTargetSettings = [
+                "DeploymentTargetSettingName": .plString("FREEBSD_DEPLOYMENT_TARGET"),
+                "DefaultDeploymentTarget": .plString(tripleVersion),
+                "MinimumDeploymentTarget": .plString(tripleVersion),
+                "MaximumDeploymentTarget": .plString(tripleVersion),
+            ]
+        } else {
+            deploymentTargetSettings = [:]
+        }
+
         return try [(.root, platform, [
             "Type": .plString("SDK"),
             "Version": .plString(Version(ProcessInfo.processInfo.operatingSystemVersion).zeroTrimmed.description),
@@ -114,62 +166,54 @@ struct GenericUnixSDKRegistryExtension: SDKRegistryExtension {
                     "LLVMTargetTripleEnvironment": .plString(tripleEnvironment),
                     "LLVMTargetTripleSys": .plString(operatingSystem.xcodePlatformName),
                     "LLVMTargetTripleVendor": .plString("unknown"),
-                ])
+                ].merging(deploymentTargetSettings, uniquingKeysWith: { _, new in new }))
             ]),
         ])]
     }
 }
 
 struct GenericUnixToolchainRegistryExtension: ToolchainRegistryExtension {
+    let plugin: GenericUnixPlugin
+
     func additionalToolchains(context: any ToolchainRegistryExtensionAdditionalToolchainsContext) async throws -> [Toolchain] {
         let operatingSystem = context.hostOperatingSystem
-        guard operatingSystem.createFallbackSystemToolchain else {
+        let fs = context.fs
+        guard operatingSystem.createFallbackSystemToolchain, let swift = plugin.swiftExecutablePath(fs: fs) else {
             return []
         }
 
-        let fs = context.fs
-
-        for swift in [
-            Environment.current["SWIFT_EXEC"].map(Path.init),
-            StackedSearchPath(environment: .current, fs: fs).lookup(Path("swift"))
-        ].compactMap(\.self) {
-            if fs.exists(swift) {
-                let realSwiftPath = try fs.realpath(swift).dirname.normalize()
-                let hasUsrBin = realSwiftPath.str.hasSuffix("/usr/bin")
-                let hasUsrLocalBin = realSwiftPath.str.hasSuffix("/usr/local/bin")
-                let path: Path
-                switch (hasUsrBin, hasUsrLocalBin) {
-                case (true, false):
-                    path = realSwiftPath.dirname.dirname
-                case (false, true):
-                    path = realSwiftPath.dirname.dirname.dirname
-                case (false, false):
-                    throw StubError.error("Unexpected toolchain layout for Swift installation path: \(realSwiftPath)")
-                case (true, true):
-                    preconditionFailure()
-                }
-                let llvmDirectories = try Array(fs.listdir(Path("/usr/lib")).filter { $0.hasPrefix("llvm-") }.sorted().reversed())
-                let llvmDirectoriesLocal = try Array(fs.listdir(Path("/usr/local")).filter { $0.hasPrefix("llvm") }.sorted().reversed())
-                return [
-                    Toolchain(
-                        identifier: ToolchainRegistry.defaultToolchainIdentifier,
-                        displayName: "Default",
-                        version: Version(),
-                        aliases: ["default"],
-                        path: path,
-                        frameworkPaths: [],
-                        libraryPaths: llvmDirectories.map { "/usr/lib/\($0)/lib" } + llvmDirectoriesLocal.map { "/usr/local/\($0)/lib" } + ["/usr/lib64"],
-                        defaultSettings: [:],
-                        overrideSettings: [:],
-                        defaultSettingsWhenPrimary: [:],
-                        executableSearchPaths: realSwiftPath.dirname.relativeSubpath(from: path).map { [path.join($0).join("bin")] } ?? [],
-                        testingLibraryPlatformNames: [],
-                        fs: fs)
-                ]
-            }
+        let realSwiftPath = try fs.realpath(swift).dirname.normalize()
+        let hasUsrBin = realSwiftPath.str.hasSuffix("/usr/bin")
+        let hasUsrLocalBin = realSwiftPath.str.hasSuffix("/usr/local/bin")
+        let path: Path
+        switch (hasUsrBin, hasUsrLocalBin) {
+        case (true, false):
+            path = realSwiftPath.dirname.dirname
+        case (false, true):
+            path = realSwiftPath.dirname.dirname.dirname
+        case (false, false):
+            throw StubError.error("Unexpected toolchain layout for Swift installation path: \(realSwiftPath)")
+        case (true, true):
+            preconditionFailure()
         }
-
-        return []
+        let llvmDirectories = try Array(fs.listdir(Path("/usr/lib")).filter { $0.hasPrefix("llvm-") }.sorted().reversed())
+        let llvmDirectoriesLocal = try Array(fs.listdir(Path("/usr/local")).filter { $0.hasPrefix("llvm") }.sorted().reversed())
+        return [
+            Toolchain(
+                identifier: ToolchainRegistry.defaultToolchainIdentifier,
+                displayName: "Default",
+                version: Version(),
+                aliases: ["default"],
+                path: path,
+                frameworkPaths: [],
+                libraryPaths: llvmDirectories.map { "/usr/lib/\($0)/lib" } + llvmDirectoriesLocal.map { "/usr/local/\($0)/lib" } + ["/usr/lib64"],
+                defaultSettings: [:],
+                overrideSettings: [:],
+                defaultSettingsWhenPrimary: [:],
+                executableSearchPaths: realSwiftPath.dirname.relativeSubpath(from: path).map { [path.join($0).join("bin")] } ?? [],
+                testingLibraryPlatformNames: [],
+                fs: fs)
+        ]
     }
 }
 
