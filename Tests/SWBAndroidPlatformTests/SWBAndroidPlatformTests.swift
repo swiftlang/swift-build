8716d0f11111d1ae405bf5f193dec6a7b90a9c3c
Jake Petroules <jake.petroules@apple.com>
2025-08-13 14:28:51 -0700

    Create one Android SDK per Swift SDK
    This matches what WebAssembly is doing, and avoids an issue where multiple installed Android Swift SDKs would cause the build to fail.

--- Tests/SWBAndroidPlatformTests/SWBAndroidPlatformTests.swift
+++ Tests/SWBAndroidPlatformTests/SWBAndroidPlatformTests.swift
@@ -39,7 +39,7 @@ fileprivate struct AndroidBuildOperationTests: CoreBasedTests {
                         "CODE_SIGNING_ALLOWED": "NO",
                         "DEFINES_MODULE": "YES",
                         "PRODUCT_NAME": "$(TARGET_NAME)",
-                        "SDKROOT": "android",
+                        "SDKROOT": "android.ndk",
                         "SUPPORTED_PLATFORMS": "android",
                         "ANDROID_DEPLOYMENT_TARGET": "22.0",
                         "ANDROID_DEPLOYMENT_TARGET[arch=riscv64]": "35.0",
