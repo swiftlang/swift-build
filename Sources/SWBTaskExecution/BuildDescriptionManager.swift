49cfd3851f09465ec689dcc8b9719198e9383aa2
Fabrice de Gans <fabrice@thebrowser.company>
2025-09-18 15:11:53 -0400

    Limit disabling to Windows

--- Sources/SWBTaskExecution/BuildDescriptionManager.swift
+++ Sources/SWBTaskExecution/BuildDescriptionManager.swift
@@ -177,7 +177,9 @@ package final class BuildDescriptionManager: Sendable {
     /// - Returns: A build description, or nil if cancelled.
     // TODO: Optimizations are disabled to work around a compiler bug. Remove this attribute when the bug is fixed.
     // See https://github.com/swiftlang/llvm-project/issues/11377 for details.
+    #if os(Windows)
     @_optimize(none)
+    #endif
     package static func constructBuildDescription(_ plan: BuildPlan, planRequest: BuildPlanRequest, signature: BuildDescriptionSignature, inDirectory path: Path, fs: any FSProxy, bypassActualTasks: Bool = false, planningDiagnostics: [ConfiguredTarget?: [Diagnostic]], delegate: any BuildDescriptionConstructionDelegate) async throws -> BuildDescription? {
         BuildDescriptionManager.descriptionsComputed.increment()
 
