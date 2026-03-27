18f2313bae1be6a9c6c04a893494289c97a17a78
Jake Petroules <jake.petroules@apple.com>
2026-03-27 15:26:40 -0700

    Sort plugin extensions deterministically
    rdar://173533778

---
+++ Tests/SWBUtilTests/PluginManagerTests.swift
@@ -0,0 +1,64 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift open source project
+//
+// Copyright (c) 2025 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+import Testing
+@_spi(Testing) import SWBUtil
+
+@Suite fileprivate struct PluginManagerTests {
+    protocol TestExtensionProtocol: Sendable {}
+
+    struct TestExtensionPoint: ExtensionPoint {
+        typealias ExtensionProtocol = any TestExtensionProtocol
+        static let name = "TestExtensionPoint"
+    }
+
+    struct ExtA: TestExtensionProtocol {}
+    struct ExtB: TestExtensionProtocol {}
+    struct ExtC: TestExtensionProtocol {}
+    struct ExtD: TestExtensionProtocol {}
+    struct ExtE: TestExtensionProtocol {}
+    struct ExtF: TestExtensionProtocol {}
+    struct ExtG: TestExtensionProtocol {}
+    struct ExtH: TestExtensionProtocol {}
+
+    @Test
+    @PluginExtensionSystemActor
+    func extensionsHaveStableOrdering() async {
+        let allExtensions: [any TestExtensionProtocol] = [
+            ExtA(), ExtB(), ExtC(), ExtD(),
+            ExtE(), ExtF(), ExtG(), ExtH(),
+        ]
+
+        var firstResult: [String]?
+
+        for _ in 0..<10 {
+            let manager = MutablePluginManager(pluginLoadingFilter: { _ in true })
+            manager.registerExtensionPoint(TestExtensionPoint())
+
+            var shuffled = allExtensions
+            shuffled.shuffle()
+
+            for ext in shuffled {
+                manager.register(ext, type: TestExtensionPoint.self)
+            }
+
+            let result = manager.extensions(of: TestExtensionPoint.self)
+            let descriptions = result.map { String(reflecting: type(of: $0)) }
+
+            if let firstResult {
+                #expect(descriptions == firstResult)
+            } else {
+                firstResult = descriptions
+            }
+        }
+    }
+}
